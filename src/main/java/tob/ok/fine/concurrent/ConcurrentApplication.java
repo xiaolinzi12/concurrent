6e5eebf8997771befb49e797b17eb0ca03ec4256
zhenglin <zhengl@quantum1tech.com>
2019-08-18 15:38:05 +0800

    init

---
+++ src/main/java/tob/ok/fine/concurrent/ConcurrentApplication.java
@@ -0,0 +1,13 @@
+package tob.ok.fine.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentApplication.class, args);
+	}
+
+}
